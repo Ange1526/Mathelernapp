b0f987671e19b8302ed5dfa99ef09fb16c294715
Ange1526 <angelicas_08@icloud.com>
2026-06-28 15:32:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,8 @@ def logout():
     logout_user()
     return redirect(url_for("index"))
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context(): 
-        db.create_all()
     app.run(debug=True)
